b33782984fe4c81cf4a88f86b2e6c1aeded37471
Muhammad Taufik <MKM02120@ptmkm.co.id>
2024-07-12 14:17:28 +0700

    done create view

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Dropdown;
+use App\Mail\AccessRequestMail;
+use Illuminate\Support\Facades\Mail;
+
+
+class AuthController extends Controller
+{
+    public function login(){
+        $dropdown = Dropdown::where('category','Role')
+        ->get();
+        return view('auth.login',compact('dropdown'));
+    }
+
+    public function postLogin(Request $request){
+        $emailOrName = $request->input('email');
+        $password = $request->input('password');
+
+        $isEmail = filter_var($emailOrName, FILTER_VALIDATE_EMAIL);
+        $credentials = $isEmail ? ['email' => $emailOrName] : ['name' => $emailOrName];
+        $credentials['password'] = $password;
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            if ($user->is_active == '1') {
+                // Update last login
+                User::where('email', $user->email)->update([
+                    'last_login' => now(),
+                    'login_counter' => $user->login_counter + 1,
+                ]);
+
+                // Redirect to the intended route after login
+                return redirect()->intended('/home');
+            } else {
+                return redirect('/')->with('statusLogin', 'Give Access First to User');
+            }
+        } else {
+            return redirect('/')->with('statusLogin', 'Wrong Email/Name or Password');
+        }
+    }
+
+
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/')->with('statusLogout','Success Logout');
+    }
+
+    public function requestAccess(Request $request)
+    {
+        // Validate the incoming request data
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            'role' => 'required|string|max:255',
+            'purpose' => 'required|string|max:255',
+        ]);
+
+        // Send the email
+        Mail::to(['aditia@ptmkm.co.id','muhammad.taufik@ptmkm.co.id','prasetyo@ptmkm.co.id'])
+            ->cc('bayu@ptmkm.co.id')
+            ->send(new AccessRequestMail($request->all()));
+
+        // Optionally, you can flash a success message or redirect to a specific page
+        return back()->with('statusLogin', 'Your request has been submitted.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DropdownController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RulesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Authentication Routes
+// Route::get('/', [AuthController::class, 'login'])->name('login');
+// Route::get('/auth/login', [AuthController::class, 'postLogin']);
+// Route::get('/auth/callback', [AuthController::class, 'handleAzureCallback']);
+// Route::get('/logout', [AuthController::class, 'logout']);
+
+Route::get('/', [AuthController::class, 'login'])->name('login');
+Route::post('/auth/login', [AuthController::class, 'postLogin']);
+Route::get('/logout', [AuthController::class, 'logout']);
+Route::post('request/access', [AuthController::class, 'requestAccess']);
+
+
+Route::middleware(['auth'])->group(function () {
+    //Home Controller
+    Route::get('/home', [HomeController::class, 'index'])->name('checksheet');
+
+    //Dropdown Controller
+    Route::get('/dropdown', [DropdownController::class, 'index'])->middleware(['checkRole:IT']);
+    Route::post('/dropdown/store', [DropdownController::class, 'store'])->middleware(['checkRole:IT']);
+    Route::patch('/dropdown/update/{id}', [DropdownController::class, 'update'])->middleware(['checkRole:IT']);
+    Route::delete('/dropdown/delete/{id}', [DropdownController::class, 'delete'])->middleware(['checkRole:IT']);
+
+    //Rules Controller
+    Route::get('/rule', [RulesController::class, 'index'])->middleware(['checkRole:IT']);
+    Route::post('/rule/store', [RulesController::class, 'store'])->middleware(['checkRole:IT']);
+    Route::patch('/rule/update/{id}', [RulesController::class, 'update'])->middleware(['checkRole:IT']);
+    Route::delete('/rule/delete/{id}', [RulesController::class, 'delete'])->middleware(['checkRole:IT']);
+
+    //User Controller
+    Route::get('/user', [UserController::class, 'index'])->middleware(['checkRole:IT']);
+    Route::post('/user/store', [UserController::class, 'store'])->middleware(['checkRole:IT']);
+    Route::post('/user/store-partner', [UserController::class, 'storePartner'])->middleware(['checkRole:IT']);
+    Route::patch('/user/update/{user}', [UserController::class, 'update'])->middleware(['checkRole:IT']);
+    Route::get('/user/revoke/{user}', [UserController::class, 'revoke'])->middleware(['checkRole:IT']);
+    Route::get('/user/access/{user}', [UserController::class, 'access'])->middleware(['checkRole:IT']);
+
+
+    Route::get('/history', [HistoricalProblemController::class, 'index']);
+    Route::get('/get-categories/{plant}', [HistoricalProblemController::class, 'getCategories']);
+    Route::get('/get-ops/{plant}/{category}', [HistoricalProblemController::class, 'getOps']);
+    Route::get('/get-location/{plant}/{category}/{op_no}', [HistoricalProblemController::class, 'getLocation']);
+
 });
