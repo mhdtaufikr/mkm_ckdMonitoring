c1cbdc84ae48ab46f0091695afb57f9238a544db
Muhammad Taufik <MKM02120@ptmkm.co.id>
2024-08-13 21:21:12 +0700

    cahnge api get data

--- app/Console/Commands/FetchInventoryItemData.php
+++ app/Console/Commands/FetchInventoryItemData.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use App\Models\InventoryItem;
 use App\Models\Inventory;
-use App\Models\PlannedInventoryItem;
 
 class FetchInventoryItemData extends Command
 {
@@ -22,7 +21,8 @@ class FetchInventoryItemData extends Command
             '65a72c7fad782dc26a0626f6',
             '617bd0ad83ef510374337d84',
             '5fc4b12bc329204cb00b56bf',
-            '65bd1017b4490c26c00a82d9'
+            '65bd1017b4490c26c00a82d9',
+            '6582ef8060c9390d890568d4'
         ];
 
         foreach ($locationIds as $locationId) {
@@ -43,54 +43,66 @@ class FetchInventoryItemData extends Command
                 $data = $response->json();
                 $items = $data['data'];
 
-                // Fetch existing inventory item IDs from the database for this location
-                $existingInventoryItemIds = InventoryItem::whereIn('inventory_id', Inventory::where('location_id', $locationId)->pluck('_id'))->pluck('_id')->toArray();
-                $fetchedInventoryItemIds = [];
+                // Fetch existing inventory items for the specific location
+                $existingInventoryItems = InventoryItem::whereIn('inventory_id', Inventory::where('location_id', $locationId)->pluck('_id'))
+                    ->get()
+                    ->keyBy('_id');
 
-                foreach ($items as $item) {
-                    // Find the corresponding inventory by code
-                    $inventory = Inventory::where('code', $item['code'])->first();
+                    foreach ($items as $item) {
+                        // Find the corresponding inventory by code and location_id
+                        $inventory = Inventory::where('code', $item['code'])->where('location_id', $locationId)->first();
 
-                    if ($inventory) {
-                        // Extract rack_type from the first word of rack
-                        $rackType = explode(' ', $item['rack'])[0];
+                        if ($inventory) {
+                            // Extract rack_type from the first word of rack
+                            $rackType = explode(' ', $item['rack'])[0];
 
-                        // Get the vendor name from the custom_field_product or default to SENOPATI for specific location IDs
-                        $vendorName = $item['cutting_center'] ?? null;
+                            // Determine if the item exists in the current location's inventory items
+                            $existingInventoryItem = InventoryItem::where('_id', $item['_id'])
+                                ->where('inventory_id', $inventory->_id)
+                                ->first();
 
-                        if (in_array($locationId, ['65a72c7fad782dc26a0626f6', '617bd0ad83ef510374337d84'])) {
-                            $vendorName = $vendorName ?? 'SENOPATI';
+                            if ($existingInventoryItem) {
+                                // If it exists, update the existing record
+                                $existingInventoryItem->update([
+                                    'serial_number' => $item['serial_number'] ?? null,
+                                    'rack' => $item['rack'] ?? null,
+                                    'rack_type' => $rackType ?? null,
+                                    'qty' => $item['qty'] ?? 0,
+                                    'status' => $item['status'] ?? null,
+                                    'receiving_date' => $item['receive_date'] ?? null,
+                                    'refNumber' => $item['refNumber'] ?? null,
+                                    'is_out' => $item['is_out'] ?? false,
+                                    'vendor_name' => $this->getVendorName($item, $locationId),
+                                    'updated_at' => $item['updated_at'] ?? now(),
+                                    'created_at' => $item['created_at'] ?? now()
+                                ]);
+                            } else {
+                                // If it doesn't exist, create a new record
+                                InventoryItem::create([
+                                    '_id' => $item['_id'],
+                                    'inventory_id' => $inventory->_id,
+                                    'serial_number' => $item['serial_number'] ?? null,
+                                    'rack' => $item['rack'] ?? null,
+                                    'rack_type' => $rackType ?? null,
+                                    'qty' => $item['qty'] ?? 0,
+                                    'status' => $item['status'] ?? null,
+                                    'receiving_date' => $item['receive_date'] ?? null,
+                                    'refNumber' => $item['refNumber'] ?? null,
+                                    'is_out' => $item['is_out'] ?? false,
+                                    'vendor_name' => $this->getVendorName($item, $locationId),
+                                    'updated_at' => $item['updated_at'] ?? now(),
+                                    'created_at' => $item['created_at'] ?? now()
+                                ]);
+                            }
+
+                            Log::info('Inventory Item Processed: ', ['inventory_item' => $item['_id']]);
                         }
-
-                        if ($locationId == '6582ef8060c9390d890568d4') {
-                            $vendorName = $vendorName ?? 'MKM';
-                        }
-
-                        $inventoryItem  = InventoryItem::updateOrCreate(
-                            ['_id' => $item['_id']],
-                            [
-                                'inventory_id' => $inventory->_id,
-                                'serial_number' => $item['serial_number'] ?? null,
-                                'rack' => $item['rack'] ?? null,
-                                'rack_type' => $rackType ?? null,
-                                'qty' => $item['qty'] ?? 0,
-                                'status' => $item['status'] ?? null,
-                                'receiving_date' => $item['receive_date'] ?? null,
-                                'refNumber' => $item['refNumber'] ?? null,
-                                'is_out' => $item['is_out'] ?? false,
-                                'vendor_name' => $vendorName,
-                                'updated_at' => $item['updated_at'] ?? null,
-                                'created_at' => $item['created_at'] ?? null
-                            ]
-                        );
-                        Log::info('Inventory Item Created/Updated: ', ['inventory_item' => $inventoryItem]);
-                        // Add the fetched inventory item ID to the array
-                        $fetchedInventoryItemIds[] = $item['_id'];
                     }
-                }
+
+
 
                 // Log fetched inventory item IDs (for debugging purposes)
-                Log::info('Fetched inventory item IDs for location ' . $locationId, $fetchedInventoryItemIds);
+                Log::info('Fetched inventory item IDs for location ' . $locationId, array_keys($existingInventoryItems->toArray()));
             } else {
                 Log::error('Failed to fetch inventory item data for location ' . $locationId, [
                     'response_body' => $response->body()
@@ -99,5 +111,20 @@ class FetchInventoryItemData extends Command
         }
 
         $this->info('Inventory item data fetched and stored successfully.');
+    }
+
+    private function getVendorName($item, $locationId)
+    {
+        $vendorName = $item['cutting_center'] ?? null;
+
+        if (in_array($locationId, ['65a72c7fad782dc26a0626f6', '617bd0ad83ef510374337d84'])) {
+            return $vendorName ?? 'SENOPATI';
+        }
+
+        if ($locationId == '6582ef8060c9390d890568d4') {
+            return $vendorName ?? 'MKM';
+        }
+
+        return $vendorName;
     }
 }
--- app/Models/InventoryItem.php
+++ app/Models/InventoryItem.php
@@ -9,11 +9,12 @@ class InventoryItem extends Model
 {
     use HasFactory;
 
-    protected $primaryKey = '_id';
-    public $incrementing = false;
+    protected $primaryKey = 'id'; // Use 'id' as the primary key
+
+    public $incrementing = true; // id is an auto-incrementing integer
 
     protected $fillable = [
-        '_id', 'inventory_id', 'serial_number', 'rack', 'rack_type', 'qty', 'status', 'receiving_date', 'refNumber', 'is_out', 'updated_at', 'created_at','vendor_name'
+        '_id', 'inventory_id', 'serial_number', 'rack', 'rack_type', 'qty', 'status', 'receiving_date', 'refNumber', 'is_out', 'updated_at', 'created_at', 'vendor_name'
     ];
 
     public function inventory()
@@ -21,4 +22,3 @@ class InventoryItem extends Model
         return $this->belongsTo(Inventory::class, 'inventory_id', '_id');
     }
 }
-
