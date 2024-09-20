9a458135cc9ffa84c9c8a40a3423ab5380d34499
Muhammad Taufik <MKM02120@ptmkm.co.id>
2024-09-20 08:01:25 +0700

    done adjustment

--- resources/views/delivery/trigger_download.blade.php
+++ resources/views/delivery/trigger_download.blade.php
@@ -5,17 +5,23 @@
 </head>
 <body>
     <script>
-        // Automatically trigger the PDF download
         window.onload = function() {
             var downloadUrl = "{{ route('delivery-note.pdf', ['id' => encrypt($deliveryNote->id)]) }}";
             window.open(downloadUrl, '_blank'); // Open PDF in a new tab or download it directly
 
-            // After the download, redirect to the CKD stamping index page
+            // After the download, redirect based on the authenticated user
             setTimeout(function() {
-                window.location.href = "{{ route('delivery-note.index.manual') }}";
+                @auth
+                    @if(auth()->user()->name == 'suparman')
+                        window.location.href = "{{ route('delivery-note.index.manual') }}";
+                    @else
+                        window.location.href = "{{ route('delivery-note.index') }}";
+                    @endif
+                @endauth
             }, 5000); // Delay the redirect slightly to allow the download to start
         };
     </script>
+
     <p>Your download will begin shortly. You will be redirected after the download completes.</p>
 </body>
 </html>
